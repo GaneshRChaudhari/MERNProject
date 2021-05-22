c2797f8018dd600f49e35194b80ed2458de4a28f
chaudharigr17.is@gmail.com <chaudharigr17.is@gmail.com>
2021-05-22 23:05:44 +0530

    Initial commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const app = express()
 
-app.get('/', (req, res) => res.send('API is Running....'))
+app.get('/', (req, res) => res.send('API is Running very well....'))
 
 const PORT = 5000
 
